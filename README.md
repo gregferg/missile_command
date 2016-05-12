2838577423608b5d002d42366b796b8c04ba047c
gregferg <gregferg@gmail.com>
2016-05-11 22:41:38 -0700

    cleared extra files

---
+++ README.md
@@ -0,0 +1,5 @@
+##Missile Command
+
+Click to fire a Missile
+Protect your bases
+UNLIMITED ROUNDS!
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   context: __dirname,
-  entry: "./lib/missle_command.js",
+  entry: "./lib/missile_command.js",
   output: {
     filename: "./lib/bundle.js"
   },
